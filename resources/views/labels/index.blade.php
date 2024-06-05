c35255e03a43abc69d3ec53dcf651410112d287d
Philip Thompson <philip.thompson.dev@gmail.com>
2024-06-05 17:26:06 -0400

    styles for notes on index and labels index

--- resources/views/labels/index.blade.php
+++ resources/views/labels/index.blade.php
@@ -4,40 +4,47 @@
             @csrf
 
             <label>
-                <input type="text" name="label">
+                <input type="text" name="label" class="w-full mb-2 p-2 border border-slate-300 rounded-md shadow focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50">
             </label>
             <x-input-error :messages="$errors->get('label')" class="mt-2" />
             <x-primary-button class="mt-4">{{ __('New Label') }}</x-primary-button>
         </form>
     </div>
 
-    <div class="mt-6 bg-white shadow-sm rounded-lg divide-y">
+    <div class="grid grid-cols-6 gap-4 mx-10 my-5">
         @foreach ($labels as $label)
-            <p class="mt-4 text-lg text-gray-900">{{ $label->label }} <small class="ml-2 text-sm text-gray-600">Created {{ $label->created_at }}</small></p>
+            <div class="col-span-1 bg-slate-50 rounded-md border border-slate-300 shadow px-4 py-4">
+                <div class="flex">
+                    <p class="text-lg text-gray-900 w-5/6">{{ $label->label }} <small class="ml-2 text-sm text-gray-600"></p>
 
-            <x-dropdown>
-                <x-slot name="trigger">
-                    <button>
-                        <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 text-gray-400" viewBox="0 0 20 20" fill="currentColor">
-                            <path d="M6 10a2 2 0 11-4 0 2 2 0 014 0zM12 10a2 2 0 11-4 0 2 2 0 014 0zM16 12a2 2 0 100-4 2 2 0 000 4z" />
-                        </svg>
-                    </button>
-                </x-slot>
-                <x-slot name="content">
-                    {{-- TODO: Users can only edit their labels --}}
+                    <div class="w-1/6 pt-2 float-end">
+                        <x-dropdown>
+                            <x-slot name="trigger">
+                                <button>
+                                    <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 text-gray-400" viewBox="0 0 20 20" fill="currentColor">
+                                        <path d="M6 10a2 2 0 11-4 0 2 2 0 014 0zM12 10a2 2 0 11-4 0 2 2 0 014 0zM16 12a2 2 0 100-4 2 2 0 000 4z" />
+                                    </svg>
+                                </button>
+                            </x-slot>
+                            <x-slot name="content">
+                                {{-- TODO: Users can only edit their labels --}}
 
-                    <x-dropdown-link :href="route('labels.edit', $label)">
-                        {{ __('Edit') }}
-                    </x-dropdown-link>
-                    <form method="POST" action="{{ route('labels.destroy', $label) }}">
-                        @csrf
-                        @method('delete')
-                        <x-dropdown-link :href="route('labels.destroy', $label)" onclick="event.preventDefault(); this.closest('form').submit();">
-                            {{ __('Delete') }}
-                        </x-dropdown-link>
-                    </form>
-                </x-slot>
-            </x-dropdown>
+                                <x-dropdown-link :href="route('labels.edit', $label)">
+                                    {{ __('Edit') }}
+                                </x-dropdown-link>
+                                <form method="POST" action="{{ route('labels.destroy', $label) }}">
+                                    @csrf
+                                    @method('delete')
+                                    <x-dropdown-link :href="route('labels.destroy', $label)" onclick="event.preventDefault(); this.closest('form').submit();">
+                                        {{ __('Delete') }}
+                                    </x-dropdown-link>
+                                </form>
+                            </x-slot>
+                        </x-dropdown>
+                    </div>
+                </div>
+                Created {{ $label->created_at }}</small>
+            </div>
         @endforeach
 
 
--- resources/views/notes/index.blade.php
+++ resources/views/notes/index.blade.php
@@ -26,43 +26,49 @@
 
         @foreach ($notes as $note)
             <div class="bg-amber-200 rounded-lg px-2 py-2">
-                <div class="">
-                    @if ($note->user->is(auth()->user()))
-                        <x-dropdown>
-                            <x-slot name="trigger">
-                                <button>
-                                    <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 text-gray-400" viewBox="0 0 20 20" fill="currentColor">
-                                        <path d="M6 10a2 2 0 11-4 0 2 2 0 014 0zM12 10a2 2 0 11-4 0 2 2 0 014 0zM16 12a2 2 0 100-4 2 2 0 000 4z" />
-                                    </svg>
-                                </button>
-                            </x-slot>
-                            <x-slot name="content">
-                                <x-dropdown-link :href="route('notes.edit', $note)">
-                                    {{ __('Edit') }}
-                                </x-dropdown-link>
-                                <form method="POST" action="{{ route('notes.destroy', $note) }}">
-                                    @csrf
-                                    @method('delete')
-                                    <x-dropdown-link :href="route('notes.destroy', $note)" onclick="event.preventDefault(); this.closest('form').submit();">
-                                        {{ __('Delete') }}
-                                    </x-dropdown-link>
-                                </form>
-                            </x-slot>
-                        </x-dropdown>
-                    @endif
+                <div class="flex">
+                    <h3 class="w-5/6 font-bold">{{ $note->title }}</h3>
+                    <div class="w-1/6">
+                        <div class="float-end">
+                            @if ($note->user->is(auth()->user()))
+                                <x-dropdown>
+                                    <x-slot name="trigger">
+                                        <button>
+                                            <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 text-gray-400" viewBox="0 0 20 20" fill="currentColor">
+                                                <path d="M6 10a2 2 0 11-4 0 2 2 0 014 0zM12 10a2 2 0 11-4 0 2 2 0 014 0zM16 12a2 2 0 100-4 2 2 0 000 4z" />
+                                            </svg>
+                                        </button>
+                                    </x-slot>
+                                    <x-slot name="content">
+                                        <x-dropdown-link :href="route('notes.edit', $note)">
+                                            {{ __('Edit') }}
+                                        </x-dropdown-link>
+                                        <form method="POST" action="{{ route('notes.destroy', $note) }}">
+                                            @csrf
+                                            @method('delete')
+                                            <x-dropdown-link :href="route('notes.destroy', $note)" onclick="event.preventDefault(); this.closest('form').submit();">
+                                                {{ __('Delete') }}
+                                            </x-dropdown-link>
+                                        </form>
+                                    </x-slot>
+                                </x-dropdown>
+                            @endif
+                        </div>
+                    </div>
                 </div>
 
-                <h3>{{ $note->title }}</h3>
+
                 <p>{{ $note->message }}</p>
 
+                {{-- TODO: Fix label delete on Note Index --}}
                 <div class="flex flex-wrap gap-1">
                     @foreach($note->labels as $label)
-                        <form method="POST" action="{{ route('labels.destroy', $label) }}">
+                        <form method="POST" action="{{ route('labels.update', $note) }}">
                             @csrf
-                            @method('delete')
+                            @method('patch')
                             <x-notes.label
                                 :label="$label->label"
-                                :href="route('labels.destroy', $label)"
+                                :href="route('labels.update', $note)"
                                 onclick="
                                     event.preventDefault();
                                     this.closest('form').submit();
