2d71439060c372472adce95f15db6fceab938967
Philip Thompson <philip.thompson.dev@gmail.com>
2024-07-15 15:26:37 -0400

    added intertia and react

--- routes/web.php
+++ routes/web.php
@@ -1,34 +1,27 @@
 <?php
 
-use App\Http\Controllers\ApiController;
-use App\Http\Controllers\LabelController;
-use App\Http\Controllers\NoteController;
 use App\Http\Controllers\ProfileController;
+use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
 
 Route::get('/', function () {
-    return view('welcome');
+    return Inertia::render('Welcome', [
+        'canLogin' => Route::has('login'),
+        'canRegister' => Route::has('register'),
+        'laravelVersion' => Application::VERSION,
+        'phpVersion' => PHP_VERSION,
+    ]);
 });
 
 Route::get('/dashboard', function () {
-    return view('dashboard');
+    return Inertia::render('Dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-    Route::post('/profile', [ApiController::class, 'create_token'])->name('api.token');
 });
 
-Route::resource('notes', NoteController::class)
-    ->only(['index', 'store', 'edit', 'update', 'destroy'])
-    ->middleware(['auth', 'verified']);
-
-Route::resource('labels', LabelController::class)
-    ->only(['index', 'store', 'edit', 'update', 'destroy'])
-    ->middleware(['auth', 'verified']);
-
 require __DIR__.'/auth.php';
-
-
