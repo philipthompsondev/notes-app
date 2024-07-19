22cdf3b31924fa0a4a07430043bc14591961546f
Philip Thompson <philip.thompson.dev@gmail.com>
2024-07-19 10:53:09 -0400

    work on label and notes index

--- resources/js/Pages/Labels/Index.jsx
+++ resources/js/Pages/Labels/Index.jsx
@@ -16,7 +16,45 @@ export default function Index({ auth }) {
 
     return (
         <AuthenticatedLayout user={auth.user}>
-            <Head title="Labels" />
+            <Head title="Labels"/>
+            <div className="max-w-2xl mx-auto p-4 sm:p-6 lg:p-8">
+                <form onSubmit={submit}>
+                    <label htmlFor="label">Label Title</label>
+                    <input
+                        type="text"
+                        name="label"
+                        placeholder="Note Title"
+                        onChange={e => setData('label', e.target.value)}
+                        className="w-full mb-2 p-2 border border-slate-300 rounded-md shadow focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"></input>
+
+                    <div className="grid grid-cols-2">
+                        <div className="col-span-1">
+                            <label htmlFor="bg_color">Background Color:</label>
+                            <input
+                                name="bg_color"
+                                id="bg_color"
+                                type="color"
+                                value="#FFFFFF"
+                                onChange={e => setData('bg_color', e.target.value)}/>
+                        </div>
+
+                        <div className="col-span-1">
+                            <label htmlFor="font_color">Font Color:</label>
+                            <input
+                                name="font_color"
+                                id="font_color"
+                                type="color"
+                                value="#000000"
+                                onChange={e => setData('font_color', e.target.value)}/>
+                        </div>
+                    </div>
+
+                    <InputError message={errors.label} className="mt-2"/>
+                    <InputError message={errors.bg_color} className="mt-2"/>
+                    <InputError message={errors.font_color} className="mt-2"/>
+                    <PrimaryButton className="mt-4" disabled={processing}>New Label</PrimaryButton>
+                </form>
+            </div>
 
         </AuthenticatedLayout>
     );
--- resources/js/Pages/Notes/Index.jsx
+++ resources/js/Pages/Notes/Index.jsx
@@ -18,6 +18,55 @@ export default function Index({ auth }) {
         <AuthenticatedLayout user={auth.user}>
             <Head title="Notes" />
 
+            <div className="grid grid-cols-5 gap-4 mx-10 my-5">
+                <div className="col-span-2 row-span-2 bg-slate-200 rounded-lg px-2 py-2">
+                    <form onSubmit={submit}>
+                        <input
+                            type="text"
+                            name="title"
+                            placeholder="Note Title"
+                            onChange={e => setData('title', e.target.value)}
+                            className="w-full mb-2 p-2 border border-slate-300 rounded-md shadow focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"></input>
+
+                        <textarea
+                            name="message"
+                            placeholder="Write something down."
+                            value={data.message}
+                            onChange={e => setData('message', e.target.value)}
+                            className="w-full h-full p-2 mb-2  border border-slate-300 rounded-md shadow focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+                        ></textarea>
+
+                        <div className="grid grid-cols-2">
+                            <div className="col-span-1">
+                                <label htmlFor="bg_color">Background Color:</label>
+                                <input
+                                    name="bg_color"
+                                    id="bg_color"
+                                    type="color"
+                                    value="#FFFFFF"
+                                    onChange={e => setData('bg_color', e.target.value)}/>
+                            </div>
+
+                            <div className="col-span-1">
+                                <label htmlFor="font_color">Font Color:</label>
+                                <input
+                                    name="font_color"
+                                    id="font_color"
+                                    type="color"
+                                    value="#000000"
+                                    onChange={e => setData('font_color', e.target.value)}/>
+                            </div>
+                        </div>
+
+                        <InputError message={errors.title} className="mt-2"/>
+                        <InputError message={errors.message} className="mt-2"/>
+                        <InputError message={errors.bg_color} className="mt-2"/>
+                        <InputError message={errors.font_color} className="mt-2"/>
+                        <PrimaryButton className="mt-4" disabled={processing}>New Note</PrimaryButton>
+                    </form>
+                </div>
+            </div>
+
         </AuthenticatedLayout>
     );
 }
