fe6c38a42f67524ded11bfaf147e31cca27256e0
Philip Thompson <philip.thompson.dev@gmail.com>
2024-07-25 17:44:15 -0400

    Updated components

--- resources/js/Components/Label.jsx
+++ resources/js/Components/Label.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function Label({ label }) {
     return (
-        <div className="rounded-lg px-2 py-2">
+        <div className="rounded-lg px-2 py-2" style={{backgroundColor: label.bg_color, color: label.font_color}}>
             <div className="flex">
                 <h3 className="w-5/6 font-bold">{label.label}</h3>
             </div>
--- resources/js/Components/Note.jsx
+++ resources/js/Components/Note.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function Note({ note }) {
     return (
-        <div className="rounded-lg px-2 py-2">
+        <div className="rounded-lg px-2 py-2" style={{backgroundColor: note.bg_color, color: note.font_color}}>
             <div className="flex">
                 <h3 className="w-5/6 font-bold">{note.title}</h3>
                 <p>{note.message}</p>
